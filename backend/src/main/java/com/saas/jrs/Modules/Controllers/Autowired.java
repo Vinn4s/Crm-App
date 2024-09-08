64a4773fefeb3561bb35da2cc2c18ecd28d47c9a
Vinnícius Yuri <vinnyprado1402@gmail.com>
2024-09-08 00:15:43 -0300

    Adiciona novos arquivos e modifica configuração
    - Adiciona controladores, entidades e serviços para vendas
    - Atualiza propriedades de configuração e Docker Compose
    
     Your branch is up to date with 'origin/main'.
     On branch main
    
     Changes to be committed:
    	new file:   backend/src/main/java/com/saas/jrs/Modules/Controllers/Autowired.java
    	new file:   backend/src/main/java/com/saas/jrs/Modules/Controllers/VendaController.java
    	new file:   backend/src/main/java/com/saas/jrs/Modules/Entitys/VendaEntity.java
    	new file:   backend/src/main/java/com/saas/jrs/Modules/Repositories/VendaRepository.java
    	new file:   backend/src/main/java/com/saas/jrs/Modules/Services/VendaServico.java
    	modified:   backend/src/main/resources/application.properties
    	modified:   docker-compose.yml
    	new file:   package-lock.json

---
+++ backend/src/main/java/com/saas/jrs/Modules/Controllers/Autowired.java
@@ -0,0 +1,5 @@
+package com.saas.jrs.Modules.Controllers;
+
+public @interface Autowired {
+
+}
---
+++ backend/src/main/java/com/saas/jrs/Modules/Entitys/VendaEntity.java
@@ -0,0 +1,52 @@
+package com.saas.jrs.Modules.Entitys;
+import java.time.LocalDateTime;
+
+public class VendaEntity {
+    private Long id;
+
+    private String produto;
+    private Double valor;
+    private String vendedor;
+    private LocalDateTime dataVenda;
+
+   //Gets e Sets
+   
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+    this.id = id;
+}
+
+    public String getProduto() {
+    return produto;
+}
+
+public void setProduto(String produto) {
+    this.produto = produto;
+}
+public double getValor() {
+    return valor;
+}
+public void setValor(double valor) {
+    
+}
+
+public String getVendedor() {
+    return vendedor;
+}
+
+public void setVendedor(String vendedor) {
+    this.vendedor = vendedor;
+}
+
+public LocalDateTime getDataVenda() {
+    return dataVenda;
+}
+
+public void setDataVenda(LocalDateTime dataVenda) {
+    this.dataVenda = dataVenda;
+}
+};
+    
